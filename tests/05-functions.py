91ef44c3abdbed72f7ca57696105bc1377528910
Esternex <idontknowbruv@yahoo.com>
2026-08-09 21:36:47 +0300

    Add functions for addition and multiplication

---
+++ tests/05-functions.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+result1 = add(10, 20)
+result2 = multiply(5, 6)
+
+print("Addition:", str(result1))
+print("Multiplication:", str(result2))

f17a0a5de2a4c161cf03f3f90088f1f12c51709e
Esternex <idontknowbruv@yahoo.com>
2026-08-09 21:24:45 +0300

    Add math operations script for area and powers

---
+++ Demo Scripts/03-math.py
@@ -0,0 +1,10 @@
+import math
+
+radius = 10
+
+area = math.pi * radius * radius
+
+print("Radius:", str(radius))
+print("Area:", str(area))
+print("Square root:", str(math.sqrt(144)))
+print("Power:", str(math.pow(2, 8)))

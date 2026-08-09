d3f2d1741d7f71c452a481ea8b47895860a8b941
Esternex <idontknowbruv@yahoo.com>
2026-08-09 21:36:55 +0300

    Create 06-lists.py

---
+++ tests/06-lists.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]
+
+print("First:", str(numbers[0]))
+print("Last:", str(numbers[4]))
+
+numbers.append(6)
+
+print("Length:", str(len(numbers)))
+
+for number in numbers:
+    print(str(number))
